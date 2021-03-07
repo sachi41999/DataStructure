8c9fa064d5271fc28207d5dab0ed2cf092eda9e4
sachi41999 <sachisabya41999@gmail.com>
2021-03-07 18:22:36 +0530

    commit

---
+++ DataStructure/src/arrays/Find_three_largest_element.java
@@ -0,0 +1,36 @@
+package arrays;
+
+public class Find_three_largest_element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//find three largest elemet from an array time:-O(n) and space:-O(1);
+		//but it doesn't print the duplicate value;
+		int arr[]= {12,34,98,23,11,83,24,98};
+		int n=arr.length;
+		
+		int first,second,third;
+		first=second=third=Integer.MIN_VALUE;
+		
+		for(int i=0;i<n;i++) {
+			if(arr[i]>=first) {
+				first=arr[i];
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			if(arr[i]>=second && arr[i]<first) {
+				second=arr[i];
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			if(arr[i]>=third && arr[i]<second) {
+				third=arr[i];
+			}
+		}
+		
+		System.out.print(first+" "+second+" "+third);
+	}
+
+}
